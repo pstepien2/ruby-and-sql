df864bef6cf118e6d42089ca850c94b69df659f0
Patrick Stepien <patrick.stepien@kellogg.northwestern.edu>
2023-04-28 21:16:06 +0000

    in_class

--- labs/3-associations.rb
+++ labs/3-associations.rb
@@ -17,8 +17,38 @@ Activity.destroy_all
 # 1. insert 3 rows in the activities table with relationships to
 # a single salesperson and 2 different contacts
 
+sp = Salesperson.find_by({"first_name"=>"Hanna"})
+c1 = Contact.find_by({"first_name"=>"Tim"})
+c2 = Contact.find_by({"first_name"=>"Craig"})
+
+new_activity = Activity.new
+new_activity["salesperson_id"] = sp["id"]
+new_activity["contact_id"] = c1["id"]
+new_activity["note"] = "Grabbed lunch with Tim"
+new_activity.save
+
+new_activity = Activity.new
+new_activity["salesperson_id"] = sp["id"]
+new_activity["contact_id"] = c2["id"]
+new_activity["note"] = "Tried to sell to Craig but he said no"
+new_activity.save
+
+new_activity = Activity.new
+new_activity["salesperson_id"] = sp["id"]
+new_activity["contact_id"] = c1["id"]
+new_activity["note"] = "Talked to Tim about firing Craig"
+new_activity.save
+
 # 2. Display all the activities between the salesperson used above
 # and one of the contacts (sample output below):
+
+puts "Activities between #{sp["first_name"]} and #{c1["first_name"]} #{c1["last_name"]}:"
+
+sp_acts_small = Activity.where({"salesperson_id"=>sp["id"], "contact_id"=>c1["id"]})
+
+for i in sp_acts_small
+    puts "- #{i["note"]}"
+end
 
 # ---------------------------------
 # Activities between Ben and Tim Cook:
@@ -28,6 +58,13 @@ Activity.destroy_all
 # CHALLENGE:
 # 3. Similar to above, but display all of the activities for the salesperson
 # across all contacts (sample output below):
+puts "#{sp["first_name"]}'s Activities:"
+
+sp_acts = Activity.where({"salesperson_id"=>sp["id"]})
+
+for i in sp_acts
+    puts "- #{i["note"]}"
+end
 
 # ---------------------------------
 # Ben's Activities:
